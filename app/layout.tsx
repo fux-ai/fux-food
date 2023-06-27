1c9e9ecdfc17bf9f49a8564547d674cb5b6e445a
adipol1359 <aditv1337@gmail.com>
2023-06-27 19:15:42 +0200

    feat: add background color to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className={poppins.variable}>
-			<body>{children}</body>
+			<body className="bg-[#f9fff6]">{children}</body>
 		</html>
 	);
 }
